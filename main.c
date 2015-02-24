5b3662a1391480b72c22b44f650516446e56bcb4
Ahmed EL-KADHI <ael-kadh@e2r3p17.42.fr>
2015-02-24 20:18:49 +0100

    main de test

--- main.c
+++ main.c
@@ -5,20 +5,35 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ael-kadh <ael-kadh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/01/23 17:15:18 by ael-kadh          #+#    #+#             */
-/*   Updated: 2015/01/23 19:32:27 by ael-kadh         ###   ########.fr       */
+/*   Created: 2015/02/24 19:36:29 by ael-kadh          #+#    #+#             */
+/*   Updated: 2015/02/24 19:40:57 by ael-kadh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 
-int		main() {
-	char	s[8] = "abc";
+int	print(int res, char *str) {
 
-	printf("before: \n%s\n", s);
-	ft_strcat(s, "tit");
-	printf("after: \n%s\n", s);
+	if (str != NULL)
+		printf("%s ==> ", str);
 
+	if (res)
+		printf("\033[32mOK\033[0m\n");
+   else
+		printf("\033[31mERROR\033[0m\n");
+
+	return res;
+}
+
+int main() {
+
+	print(ft_bzeroVerif(), "\033[36mft_bzero\33[33m");
+	print(ft_strcatVerif(), "\033[36mft_strcat\33[33m");
+	print(ft_isAlphaVerif(), "\033[36mft_isalpha\33[33m");
+	print(ft_isAlNumVerif(), "\033[36mft_isalnum\33[33m");
+
+	print(ft_strcatVerif(), "\033[36mft_strcat\33[33m");
+	print(ft_strcatVerif(), "\033[36mft_strcat\33[33m");
+	print(ft_strcatVerif(), "\033[36mft_strcat\33[33m");
 	return 0;
 }
---
+++ main.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yyang <yyang@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/12/05 18:58:31 by celegran          #+#    #+#             */
+/*   Updated: 2015/02/24 19:41:16 by ael-kadh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MAIN_H
+# define MAIN_H
+
+#include <stdio.h>
+#include <assert.h>
+#include <strings.h>
+#include <stdlib.h>
+
+int	print(int res, char *str);
+int	ft_bzeroVerif();
+int   ft_strcatVerif();
+int   ft_isAlphaVerif();
+int   ft_isAlNumVerif();
+
+#endif
